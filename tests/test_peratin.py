3b1a30e15e207fdef39eb658fbcef291a7d3f51d
Manish Thakur <manishthakur272003@gmail.com>
2025-11-05 16:51:10 +0530

    Update test_peratin.py

---
+++ tests/test_peratin.py
@@ -0,0 +1,9 @@
+from src.math_operation import add,subtract
+
+def test_add():
+    assert add(6,5)==11
+    assert add(7,4)==11
+    assert add (-1,1)==0
+def test_sub():
+    assert subtract(6,7)==1
+    assert subtract(6,7)==1
